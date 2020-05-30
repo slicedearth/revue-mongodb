ed49991b0c34f3a99fb50bc847b0405c1cd93ce8
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-05-30 22:07:27 +1000

    Challenge 13
    Added bcyrpt package to hash and salt passwords before being saved to the database.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const { User, validateUser } = require('../models/userModel');
 const router = express.Router();
 
@@ -14,11 +15,9 @@ router.post('/', async (req, res) => {
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
-  //   res.send({
-  //     name: user.name,
-  //     email: user.email,
-  //   });
   res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
