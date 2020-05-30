2d925451272f3eef337a2039241ac899f09df62e
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-05-30 20:02:47 +1000

    Challenge 9
    Added objectID validation to the rental model using joi-objectid.

--- models/rentalModel.js
+++ models/rentalModel.js
@@ -1,5 +1,6 @@
 const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
+Joi.objectId = require('joi-objectid')(Joi);
 
 // RENTAL MODEL & SCHEMA
 const Rental = mongoose.model(
@@ -62,8 +63,8 @@ const Rental = mongoose.model(
 // RENTAL VALIDATION
 validateRental = (rental) => {
   const schema = Joi.object({
-    customerId: Joi.string().required(),
-    movieId: Joi.string().required(),
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
   });
   return schema.validate(rental);
 };
