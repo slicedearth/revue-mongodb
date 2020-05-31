03733405be90583b96ca20ba96d27136b5043fb5
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-05-31 19:23:45 +1000

    Challenge 22
    Added try catch blocks to the genre routes in order to handle unexpected errors.

--- routes/genres.js
+++ routes/genres.js
@@ -7,64 +7,84 @@ const router = express.Router();
 
 // GET GENRES
 router.get('/', async (req, res) => {
-  const genres = await Genre.find().sort('name');
-  res.send(genres);
+  try {
+    const genres = await Genre.find().sort('name');
+    res.send(genres);
+  } catch (ex) {
+    res.status(500).send('Something Failed');
+  }
 });
 
 // GET GENRE BY ID
 router.get('/:id', async (req, res) => {
-  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-    const genre = await Genre.findById(req.params.id);
-    if (!genre) return res.status(404).send('Invalid Genre ID');
-    res.send(genre);
-  } else {
-    return res.status(404).send('Invalid ID Format');
+  try {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      const genre = await Genre.findById(req.params.id);
+      if (!genre) return res.status(404).send('Invalid Genre ID');
+      res.send(genre);
+    } else {
+      return res.status(404).send('Invalid ID Format');
+    }
+  } catch (ex) {
+    res.status(500).send('Something Failed');
   }
 });
 
 // POST GENRE
 router.post('/', authMiddleware, async (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
+  try {
+    const { error } = validateGenre(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
 
-  res.send(genre);
+    res.send(genre);
+  } catch (ex) {
+    res.status(500).send('Something Failed');
+  }
 });
 
 // UPDATE GENRE
 router.put('/:id', authMiddleware, async (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+  try {
+    const { error } = validateGenre(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
 
-  const genre = await Genre.findByIdAndUpdate(
-    req.params.id,
-    { name: req.body.name },
-    { new: true }
-  );
-  if (!genre) {
-    return res.status(404).send('Invalid Genre ID');
-  }
-
-  res.send(genre);
-});
-
-// DELETE GENRE
-router.delete('/:id', [authMiddleware, adminMiddleware], async (req, res) => {
-  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-    const genre = await Genre.findByIdAndRemove(req.params.id);
-
+    const genre = await Genre.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true }
+    );
     if (!genre) {
       return res.status(404).send('Invalid Genre ID');
     }
 
     res.send(genre);
-  } else {
-    return res.status(404).send('Invalid ID Format');
+  } catch (ex) {
+    res.status(500).send('Something Failed');
+  }
+});
+
+// DELETE GENRE
+router.delete('/:id', [authMiddleware, adminMiddleware], async (req, res) => {
+  try {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      const genre = await Genre.findByIdAndRemove(req.params.id);
+
+      if (!genre) {
+        return res.status(404).send('Invalid Genre ID');
+      }
+
+      res.send(genre);
+    } else {
+      return res.status(404).send('Invalid ID Format');
+    }
+  } catch (ex) {
+    res.status(500).send('Something Failed');
   }
 });
 
