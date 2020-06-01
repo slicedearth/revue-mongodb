34b012953b4975c649509414c0f89f2f2fb6184b
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-06-01 21:41:10 +1000

    Challenge 26
    Installed winston package and added it to error middleware in order to log errors to a folder.

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,4 +1,26 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'vidly-service' },
+  transports: [
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new winston.transports.File({
+      filename: 'logs/warnings.log',
+      level: 'warn',
+    }),
+    new winston.transports.File({
+      filename: 'logs/combined.log',
+    }),
+  ],
+});
+
 errorMiddleware = (err, req, res, next) => {
+  logger.error(err.message, err);
   res.status(500).send('Something Failed');
 };
 module.exports = errorMiddleware;
