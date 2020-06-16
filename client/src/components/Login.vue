bb895831137e0667f96e07a1460de084dd4a274b
slicedearth <47583576+slicedearth@users.noreply.github.com>
2020-06-17 00:25:27 +1000

    Created Login and Register Modals

---
+++ client/src/components/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <button
+      v-if="!$store.state.isUserLoggedIn"
+      @click="showModal"
+      class="btn btn-info mr-2"
+    >
+      Login
+    </button>
+    <b-modal ref="lginModal" centered title="Login" hide-footer>
+      <b-form-input
+        name="email"
+        type="email"
+        placeholder="Enter Email"
+        v-model="email"
+        class="mb-3"
+      ></b-form-input>
+      <b-form-input
+        type="password"
+        name="password"
+        placeholder="Enter Password"
+        v-model="password"
+        class="mb-3"
+      ></b-form-input>
+      <b-card-text v-html="error" class="error mt-3"
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b-card-text
+      >
+      <b-button variant="info" @click="login" class="w-100">Login</b-button>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: null,
+    };
+  },
+  methods: {
+    showModal() {
+      this.$refs['lginModal'].show();
+    },
+    async login() {
+      try {
+        const response = await AuthenticationService.login({
+          email: this.email,
+          password: this.password,
+        });
+        this.$store.dispatch('setToken', response.data);
+        this.$store.dispatch('setUser', response.data.user);
+        this.$refs['lginModal'].hide();
+        console.log(response.data.token);
+        console.log(response.data);
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
---
+++ client/src/components/Register.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <!-- Register Button -->
+    <button
+      v-if="!$store.state.isUserLoggedIn"
+      @click="showModal"
+      class="btn btn-secondary mr-2"
+    >
+      Register
+    </button>
+    <!-- Register Modal -->
+    <b-modal ref="regModal" centered title="Register" hide-footer>
+      <b-form-input
+        name="email"
+        type="email"
+        placeholder="Enter Email"
+        v-model="email"
+        class="mb-3"
+      ></b-form-input>
+      <b-form-input
+        type="text"
+        name="username"
+        placeholder="Enter Username"
+        v-model="username"
+        class="mb-3"
+      ></b-form-input>
+      <b-form-input
+        type="password"
+        name="password"
+        placeholder="Enter Password"
+        v-model="password"
+        class="mb-3"
+      ></b-form-input>
+      <b-card-text v-html="error" class="error mt-3"
+        >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b-card-text
+      >
+      <b-button variant="info" @click="register" class="w-100"
+        >Register</b-button
+      >
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService';
+export default {
+  name: 'Register',
+  data() {
+    return {
+      email: '',
+      username: '',
+      password: '',
+      error: null,
+    };
+  },
+  methods: {
+    showModal() {
+      this.$refs['regModal'].show();
+    },
+    async register() {
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          username: this.username,
+          password: this.password,
+        });
+        // console.log(this.$store);
+        this.$store.dispatch('setToken', response.data.token);
+        this.$store.dispatch('setUser', response.data.user);
+        this.$refs['regModal'].hide();
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
